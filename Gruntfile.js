9b97c5f86966cf92eab988ca3b6902dd87872f01
Alex Wilson <yourpalal2@gmail.com>
2013-08-25 15:10:34 -0600

    Add html minification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,22 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		bytesize: {
 			dist: {
-				src: ['<%= pkg.name %>.zip']
+				src: ['dist/<%= pkg.name %>.zip']
 			}
 		},
-		clean: ["optimized.js", '<%= pkg.name %>.zip'],
+		clean: ["dist/"],
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhiteSpace: true,
+					removeAttributeQuotes: true,
+				},
+				files: {
+					'dist/index.html': 'index.html'
+				}
+			}
+		},
 		jshint: {
 			all: ['Gruntfile.js', 'main.js'],
 			options: {
@@ -16,24 +28,26 @@ module.exports = function(grunt) {
 		uglify: {
 			dist: {
 				files: {
-					'optimized.js': 'main.js'
+					'dist/main.js': 'main.js'
 				}
 			}
 		},
 		zip: {
 			dist: {
-				src: ['main.js', 'index.html'],
-				dest: '<%= pkg.name %>.zip'
+				cwd: 'dist/',
+				src: ['dist/main.js', 'dist/index.html'],
+				dest: 'dist/<%= pkg.name %>.zip'
 			}
 		}
 	});
 
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-bytesize');
 	grunt.loadNpmTasks('grunt-zip');
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'zip', 'bytesize']);
+	grunt.registerTask('default', ['htmlmin', 'jshint', 'uglify', 'zip', 'bytesize']);
 };
